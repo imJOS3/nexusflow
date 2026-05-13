6f9303e33a1072f6914167299ac95ed70772f19c
imJOS3 <josebenjumea2005@gmail.com>
2026-05-13 12:12:46 -0500

    add docker-compose database

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config'; //cargar variables de entorno desde el archivo .env
 import { AppModule } from './app.module';
+
+declare const process: { env: { PORT?: string } };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,3 @@
-﻿export {};
+﻿import { PrismaClient } from '@prisma/client';
+
+export const db = new PrismaClient();
